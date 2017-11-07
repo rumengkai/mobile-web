21a2aa387d689c2313b35246166e769b49a9f931
rumengkai <rumengkai@aliyun.com>
2017-11-07 11:21:24 +0800

    danpin

--- src/m/detail/App.vue
+++ src/m/detail/App.vue
@@ -170,7 +170,7 @@
             }else{
               if (this.type===0) {
                 window.location.href="/m/channel.html?id="+this.articles.channel;
-              }else if(this.type===1){
+              }else if(this.type==1){
                 window.location.href="/m/channel-small.html?id="+this.articles.channel;
               }
             }
