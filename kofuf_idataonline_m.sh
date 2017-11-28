171d6f1f4ce317098ce1501ac9d3816e373369db
gfcj <kafka0102@163.com>
2017-11-28 18:13:14 +0800

    backups

--- kofuf_idataonline_m.sh
+++ kofuf_idataonline_m.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+date_dir=m_`date +%Y_%m_%d`
+echo ${date_dir}
+cp -r /home/gongfu/third/nginx/html/m /home/gongfu/third/nginx/html/backups/${date_dir}
+echo 'BACKUPS SUCCESS'
 rm -rf /home/gongfu/third/nginx/html/m/*
 cp -r /home/gongfu/code/mobile_web/dist/m/* /home/gongfu/third/nginx/html/m/
-echo 'SUCCESS'
+echo 'UPDATE SUCCESS'
