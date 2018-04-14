c6fe9c058374f084bdb093ee8352448d5dbfcc5c
rumengkai <rumengkai@aliyun.com>
2018-04-14 09:32:41 +0800

    https->http

--- config/index.js
+++ config/index.js
@@ -38,7 +38,7 @@ module.exports = {
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
-    // (https://github.com/webpack/css-loader#sourcemaps)
+    // (http://github.com/webpack/css-loader#sourcemaps)
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
     cssSourceMap: false
